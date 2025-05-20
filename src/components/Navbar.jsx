7adf4565b883f07df60c34cf0bf11d920c07c9a0
Jack Bryant <jb1730305@gmail.com>
2025-05-20 21:24:47 +1200

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar({cartItemCount}) {
+    return (
+        <nav className="navbar">
+            <div className="logo">sellingAllsorts</div>
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/products">Products</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+            <div className='cart-container'>
+                <div className='cart-icon'>
+                🛒 <span className="cart-count">{cartItemCount}</span>
+                </div>
+                <button className='checkout-button'>checkout</button> 
+            </div>       
+        </nav>
+    )
+}
+
+export default Navbar
